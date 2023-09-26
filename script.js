132b8773ce94f9254898ab4a6ccf97884d7e4690
khaly <khalyruot12345@gmail.com>
2023-09-26 01:57:02 -0700

    add input box

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ var six = document.querySelector('#six');
 var seven = document.querySelector('#seven');
 var eight = document.querySelector('#eight');
 var nine = document.querySelector('#nine');
+var plus = document.querySelector('#plus');
+var minus = document.querySelector('#minus');
+var multiply = document.querySelector('#multiply');
+var divide = document.querySelector('#divide');
+var comma = document.querySelector('#comma');
+
